fd41fbdbe2195b9cbf5f7b857fd008b5cd00eec3
Noa Heinrich <noah.heinrich@flatironschool.com>
2019-03-22 21:39:04 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -27,7 +27,7 @@ class Application
         @@cart << item
         resp.write "added #{item}"
       else 
-        resp.write "food not found"
+        resp.write "We don't have that item"
       end
     else
       resp.write "Path Not Found"
